8568664caa1b0ef595b55fbcb14080d37d88e8a2
NatalyaShults <126339371+NatalyaShults@users.noreply.github.com>
2023-05-02 21:17:34 +0500

    Update Radio.java

--- src/main/java/ru/netology/services/Radio.java
+++ src/main/java/ru/netology/services/Radio.java
@@ -2,11 +2,15 @@ package ru.netology.services;
 
 public class Radio {
 
+    private int currentStation;
+    private int currentVolume;
+    
     private int maxStation;
     private int minStation = 0;
-    private int currentStation;
-
-
+  
+    private int maxVolume = 100;
+    private int minVolume = 0;
+   
     public Radio() {
         this.maxStation = 9;
     }
@@ -36,15 +40,6 @@ public class Radio {
         }
         this.currentStation = newCurrentStation;
     }
-//    public void setCurrentStation(int currentStation) {
-//        if (currentStation < 0) { // если меньше 0, возвращается к 0
-//            return;
-//        }
-//        if (currentStation > 9) { // если больше 9, возвращается к 9
-//            return;
-//        }
-//        this.currentStation = currentStation;
-//    }
 
     public void nextStation() { // переключаем на следующую станцию
         if (currentStation != maxStation) { // если была меньше 9, станет +1
@@ -54,15 +49,6 @@ public class Radio {
         }
     }
 
-//    public void nextStation() { // переключаем на следующую станцию
-//        if (currentStation != 9) { // если была меньше 9, станет +1
-//            currentStation++;
-//        } else {
-//            currentStation = 0; // если была 9, станет 0
-//        }
-//    }
-
-
     public void prevStation() { // переключаем на предыдущую станцию
         if (currentStation != minStation) { // если была больше 0, станет -1
             currentStation--;
@@ -70,17 +56,6 @@ public class Radio {
             currentStation = maxStation; // если была 0, станет 9
         }
     }
-
-    //    public void prevStation() { // переключаем на предыдущую станцию
-//        if (currentStation != 0) { // если была больше 0, станет -1
-//            currentStation--;
-//        } else {
-//            currentStation = 9; // если была 0, станет 9
-//        }
-//    }
-    private int maxVolume = 100;
-    private int minVolume = 0;
-    private int currentVolume;
 
     public int getCurrentVolume() {
         return currentVolume;
@@ -104,38 +79,16 @@ public class Radio {
         this.currentVolume = currentVolume;
     }
 
-//    public void setCurrentVolume(int currentVolume) { // выбираем громкость
-//        if (currentVolume < 0) { // если меньше 0, возвращается к 0
-//            return;
-//        }
-//        if (currentVolume > 100) { // если больше 100, возвращается к 100
-//            return;
-//        }
-//        this.currentVolume = currentVolume;
-//    }
-
     public void increaseVolume() { // громкость добавляется +1
         if (currentVolume != maxVolume) { // если была меньше 100, станет +1
             currentVolume++;
         }
     }
 
-//    public void increaseVolume() { // громкость добавляется +1
-//        if (currentVolume != 100) { // если была меньше 100, станет +1
-//            currentVolume++;
-//        }
-//    }
-
     public void turnDownVolume() { // громкость уменьщается на -1
         if (currentVolume != minVolume) { // если была больше 0, станет -1
             currentVolume--;
         }
     }
-
-//    public void turnDownVolume() { // громкость уменьщается на -1
-//        if (currentVolume != 0) { // если была больше 0, станет -1
-//            currentVolume--;
-//        }
-//    }
 
 }
